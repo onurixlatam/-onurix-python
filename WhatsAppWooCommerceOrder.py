07d209ede427f9b9af5f8e13d50b71543adf5791
AlejandroZambranoInchcape <ext.jhoan.zambrano@inchcapeservices.com>
2024-10-08 00:33:06 -0500

    remove unnecessary comments

--- WhatsAppWooCommerceOrder.py
+++ WhatsAppWooCommerceOrder.py
@@ -3,13 +3,6 @@ import json
 
 url = "https://www.onurix.com/api/v1/whatsapp/woocommerce/order?key=AQUI_SU_KEY&client=AQUI_SU_CLIENT&template=AQUI_EL_NOMBRE_DE_LA_PLANTILLA"
 
-# payload = json.dumps({
-#   "phone": "+57320123457",
-#   "body": [
-#     "Parametro1",
-#     "Parametro2"
-#   ]
-# })
 payload = json.dumps('AQUI_EL_JSON_CON_LOS_VALORES_PARA_LA_PLANTILLA')
 headers = {
   'Content-Type': 'application/json'
